30061b67d984d8d453d064914d9b646b9f698e6a
jjitch <junnosuke_08@icloud.com>
2020-09-27 02:18:00 +0900

    abc109

--- sandbox/test.cpp
+++ sandbox/test.cpp
@@ -1,6 +1,24 @@
 #include <bits/stdc++.h>
+using namespace std;
 
 int main(){
-    std::cout << 111 << std::endl;
+    cout << 111 << endl;
+    priority_queue<int> pq;
+    pq.push(10);
+    pq.push(3);
+    pq.push(6);
+    pq.push(1);
+    while (!pq.empty())
+    {
+        cout << pq.top() << endl;
+        pq.pop();
+    }
+
+    set<int> S;
+    S.insert(10);
+    S.insert(6);
+    S.insert(10);
+    cout << S.size() << endl;
+    
     return 0;
 }
