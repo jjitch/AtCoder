3f1bb28b443a09f9573baad7ee30dd66c3a1bc99
jjitch <junnosuke_08@icloud.com>
2021-01-11 02:34:38 +0900

    d muzui

--- ABC188/d.py
+++ ABC188/d.py
@@ -1,1 +1,18 @@
-n, c = map(int, input().split())
+n, C = map(int, input().split())
+events=[]
+for i in range(n):
+    a, b, c = map(int, input().split())
+    events.append((a-1, c))
+    events.append((b, -c))
+
+events.sort()
+ans = 0
+fee = 0
+t = 0
+for x, f in events:
+    if t != x:
+        ans += min(C, fee) * (x - t)
+        t = x
+    fee += f
+
+print(ans)
