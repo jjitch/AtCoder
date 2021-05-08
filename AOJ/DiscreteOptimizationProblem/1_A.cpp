a9218819cc1d399f328b39f861d695a35960ac86
jjitch <junnosuke_08@icloud.com>
2021-05-08 14:04:40 +0900

    streak

--- AOJ/DiscreteOptimizationProblem/1_A.cpp
+++ AOJ/DiscreteOptimizationProblem/1_A.cpp
@@ -2,37 +2,34 @@
 using namespace std;
 using i64 = long long;
 #define rep(i, n) for (i64 i = 0; i < (n); i++)
-constexpr int INF32 = 1 << 30;
+#define IN_i64(n) \
+    i64 n; \
+    cin >> n;
+#define IN_veci64(a, n) \
+    vector<i64> a((n)); \
+    rep(i, n) cin >> a[i];
 constexpr i64 INF64 = 1LL << 62LL;
 inline i64 modpow(i64, i64, i64 = INF64);
 inline i64 gcd(i64, i64);
 template <class T> inline bool chmax(T &a, T b);
 template <class T> inline bool chmin(T &a, T b);
 
+constexpr i64 N_MAX = 50010;
+constexpr i64 M_MAX = 22;
+
 int main()
 {
-    i64 n, m;
-    cin >> n >> m;
-    vector<i64> c(m);
-    rep(i, m) cin >> c[i];
-    sort(c.begin(), c.end(), greater<i64>());
-    i64 allcase = modpow(2, m);
-    i64 ans = INF64;
-    rep(state, allcase)
-    {
-        i64 tmp = n;
-        i64 cnt = 0;
-        rep(i, m)
-        {
-            if ((state >> i) & 1)
-            {
-                cnt += tmp / c[i];
-                tmp %= c[i];
-            }
-        }
-        if (tmp == 0) chmin(ans, cnt);
+    IN_i64(n);
+    IN_i64(m);
+    IN_veci64(c, m);
+    sort(c.begin(), c.end());
+    // vector<vector<i64>> dp(C_MAX, vector<i64>(N_MAX, INF64));
+    vector<i64> dp(N_MAX,INF64);
+    dp[0]=0;
+    rep(i, m) rep(sum_v, N_MAX) {
+        if (sum_v - c[i]>=0) chmin(dp[sum_v], dp[sum_v - c[i]] + 1);
     }
-    cout << ans << endl;
+    cout<<dp[n]<<endl;
 }
 
 i64 modpow(i64 base, i64 exp, i64 mod)
