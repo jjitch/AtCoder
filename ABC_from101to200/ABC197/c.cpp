85e7d2f532a52acc0149e49befba801e7ef7dedc
jjitch <junnosuke_08@icloud.com>
2021-07-26 13:39:10 +0900

    streak

---
+++ ABC_from101to200/ABC197/c.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace atcoder;
+using namespace std;
+using i64 = long long;
+using vi = vector<i64>;
+using vvi = vector<vi>;
+#define REP(i, n) for (i64 i = 0; i < (n); i++)
+#define IN_i64(n) \
+	i64 n; \
+	cin >> n;
+#define IN_vi(a, n) \
+	vi a((n)); \
+	REP(i, n) cin >> a[i];
+#define SORT(a) sort(a.begin(), a.end())
+#define REVERSE(a) reverse(a.begin(), a.end())
+constexpr i64 INF64 = 1LL << 60LL;
+constexpr i64 MOD = 1000000007;
+inline i64 modpow(i64, i64, i64 = INF64);
+inline i64 gcd(i64, i64);
+template <class T> inline bool chmax(T &a, T b);
+template <class T> inline bool chmin(T &a, T b);
+
+int main()
+{
+	IN_i64(n);
+	IN_vi(a, n);
+	i64 ans = INF64;
+	i64 state = modpow(2, n - 1);
+	REP(c, state)
+	{
+		i64 tmp = 0;
+		i64 seq = 0;
+		REP(i, n)
+		{
+			seq |= a[i];
+			if ((c >> i) & 1)
+			{
+				tmp ^= seq;
+				seq = 0;
+			}
+		}
+		if (seq) { tmp ^= seq; }
+		chmin(ans, tmp);
+	}
+	cout << ans << endl;
+}
+
+i64 modpow(i64 base, i64 ex, i64 mod)
+{
+	i64 ans = 1;
+	while (ex > 0)
+	{
+		if (ex % 2 == 1)
+		{
+			ans *= base;
+			ans %= mod;
+		}
+		ex /= 2;
+		base *= base;
+		base %= mod;
+	}
+	return ans;
+}
+
+i64 gcd(i64 a, i64 b)
+{
+	if (b == 0) return a;
+	return gcd(b, a % b);
+}
+
+template <class T> inline bool chmax(T &a, T b)
+{
+	if (a < b)
+	{
+		a = b;
+		return true;
+	}
+	return false;
+}
+
+template <class T> inline bool chmin(T &a, T b)
+{
+	if (a > b)
+	{
+		a = b;
+		return true;
+	}
+	return false;
+}
