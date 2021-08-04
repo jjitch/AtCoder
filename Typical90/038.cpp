c6e695ee68b781109989173d20b48f4ef1c7004d
jjitch <junnosuke_08@icloud.com>
2021-08-05 01:14:52 +0900

    streak

---
+++ Typical90/038.cpp
@@ -0,0 +1,77 @@
+#define _USE_MATH_DEFINES
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace atcoder;
+using namespace std;
+using i64 = long long;
+using vi = vector<i64>;
+using vvi = vector<vi>;
+#define REP(i, n) for (i64 i = 0; i < (n); i++)
+#define IN_i64(n) \
+	i64 n; \
+	cin >> n;
+#define IN_vi(a, n) \
+	vi a((n)); \
+	REP(i, n) cin >> a[i];
+#define SORT(a) sort(a.begin(), a.end())
+#define REVERSE(a) reverse(a.begin(), a.end())
+constexpr i64 INF64 = 1LL << 60LL;
+constexpr i64 MOD = 1000000007;
+inline i64 modpow(i64, i64, i64 = INF64);
+inline i64 gcd(i64, i64);
+template <class T> inline bool chmax(T &a, T b);
+template <class T> inline bool chmin(T &a, T b);
+
+int main()
+{
+	IN_i64(a);
+	IN_i64(b);
+	i64 d = gcd(a, b);
+	if (a / d > modpow(10, 18) / b) cout << "Large" << endl;
+	else
+		cout << (a / d) * b << endl;
+}
+
+i64 modpow(i64 base, i64 ex, i64 mod)
+{
+	i64 ans = 1;
+	while (ex > 0)
+	{
+		if (ex % 2 == 1)
+		{
+			ans *= base;
+			ans %= mod;
+		}
+		ex /= 2;
+		base *= base;
+		base %= mod;
+	}
+	return ans;
+}
+
+i64 gcd(i64 a, i64 b)
+{
+	if (b == 0) return a;
+	return gcd(b, a % b);
+}
+
+template <class T> inline bool chmax(T &a, T b)
+{
+	if (a < b)
+	{
+		a = b;
+		return true;
+	}
+	return false;
+}
+
+template <class T> inline bool chmin(T &a, T b)
+{
+	if (a > b)
+	{
+		a = b;
+		return true;
+	}
+	return false;
+}
