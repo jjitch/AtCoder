87de349fa8d9b0d659f795227b85443c0493ad9e
jjitch <junnosuke_08@icloud.com>
2021-10-03 13:22:19 +0900

    streak

---
+++ ABC_from101to200/ABC199/c.cpp
@@ -0,0 +1,130 @@
+#define _USE_MATH_DEFINES
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace atcoder;
+using namespace std;
+using i64 = long long;
+using vi = vector<i64>;
+using vvi = vector<vi>;
+using pii = pair<i64, i64>;
+#define REP(i, n) for (i64 i = 0; i < static_cast<i64>(n); i++)
+#define IN_i64(n) \
+	i64 n; \
+	cin >> n;
+#define IN_vi(a, n) \
+	vi a((n)); \
+	REP(i, n) cin >> a[i];
+#define SORT(a) sort(a.begin(), a.end())
+#define REVERSE(a) reverse(a.begin(), a.end())
+constexpr i64 INF64 = 1LL << 60LL;
+constexpr i64 MOD = 1000000007;
+inline i64 modpow(i64, i64, i64 = 0);
+inline i64 modinv(i64, i64);
+inline i64 gcd(i64, i64);
+template <class T> inline bool chmax(T &a, T b);
+template <class T> inline bool chmin(T &a, T b);
+template <class T> inline void show(const vector<T> &v);
+
+int main()
+{
+	IN_i64(n);
+	string s;
+	cin >> s;
+	IN_i64(q);
+	i64 cnt = 0;
+	vector<pii> flip;
+	REP(_, q)
+	{
+		IN_i64(t);
+		IN_i64(a);
+		IN_i64(b);
+		if (t == 2)
+		{
+			cnt++;
+			continue;
+		}
+		a--;
+		b--;
+		if (cnt & 1)
+		{
+			a += n;
+			b += n;
+			if (a >= 2 * n) a -= 2 * n;
+			if (b >= 2 * n) b -= 2 * n;
+		}
+		flip.emplace_back(a, b);
+	}
+	for_each(flip.begin(), flip.end(), [&](pii p) { swap(s[p.first], s[p.second]); });
+	if (cnt & 1) rotate(s.begin(), s.begin() + n, s.end());
+	cout << s << endl;
+}
+
+i64 modpow(i64 base, i64 ex, i64 mod)
+{
+	i64 ans = 1;
+	while (ex)
+	{
+		if (ex & 1)
+		{
+			ans *= base;
+			if (mod) ans %= mod;
+		}
+		ex >>= 1;
+		base *= base;
+		if (mod) base %= mod;
+	}
+	return ans;
+}
+
+i64 modinv(i64 a, i64 mod)
+{
+	i64 b = mod;
+	i64 x = 1;
+	i64 y = 0;
+	while (b)
+	{
+		i64 t = a / b;
+		a -= t * b;
+		swap(a, b);
+		x -= t * y;
+		swap(x, y);
+	}
+	x %= mod;
+	if (x < 0) x += mod;
+	return x;
+}
+
+i64 gcd(i64 a, i64 b)
+{
+	if (b == 0) return a;
+	return gcd(b, a % b);
+}
+
+template <class T> inline bool chmax(T &a, T b)
+{
+	if (a < b)
+	{
+		a = b;
+		return true;
+	}
+	return false;
+}
+
+template <class T> inline bool chmin(T &a, T b)
+{
+	if (a > b)
+	{
+		a = b;
+		return true;
+	}
+	return false;
+}
+
+template <class T> inline void show(const vector<T> &v)
+{
+	const i64 n = static_cast<i64>(v.size()) - 1;
+	for (i64 i = 0; i < n; i++)
+		cout << v[i] << " ";
+	cout << v[n] << "\n";
+}
