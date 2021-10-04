661353cb10af63350d2bccec53f2cc33b233b9eb
jjitch <junnosuke_08@icloud.com>
2021-10-05 00:56:56 +0900

    streak

--- ABC_from101to200/ABC174/e.cpp
+++ ABC_from101to200/ABC174/e.cpp
@@ -26,49 +26,30 @@ template <class T> inline bool chmax(T &a, T b);
 template <class T> inline bool chmin(T &a, T b);
 template <class T> inline void show(const vector<T> &v);
 
-i64 dx[4] = {1, 0, -1, 0};
-i64 dy[4] = {0, 1, 0, -1};
-
 int main()
 {
-	IN_i64(h);
-	IN_i64(w);
-	vvi maze(h, vi(w));
-	REP(i, h)
+	IN_i64(n);
+	IN_i64(k);
+	IN_vi(a, n);
+	i64 r, m, l;
+	r = 0;
+	l = 1000000000 + 10;
+	m = (r + l) / 2;
+	auto solve = [&](i64 b) {
+		i64 cnt = 0;
+		REP(i, n) cnt += (a[i] + b - 1) / b - 1;
+		return cnt <= k;
+	};
+	m = (l + r) / 2;
+	while (l - r > 1 && m != r)
 	{
-		string s;
-		cin >> s;
-		REP(j, w) { maze[i][j] = s[j] == '.' ? 1 : 0; }
+		if (solve(m))
+			l = m;
+		else
+			r = m;
+		m = (l + r) / 2;
 	}
-	i64 ans = 0;
-	REP(i, h) REP(j, w)
-	{
-		if (!maze[i][j]) continue;
-		vvi initial(h, vi(w, INF64));
-		initial[i][j] = 0;
-		queue<pii> q;
-		i64 furthest = 0;
-		q.emplace(i, j);
-		while (!q.empty())
-		{
-			pii now = q.front();
-			q.pop();
-			REP(k, 4)
-			{
-				i64 idx = now.first + dx[k];
-				i64 jdy = now.second + dy[k];
-				if (idx < 0 || h <= idx || jdy < 0 || w <= jdy) continue;
-				if (!maze[idx][jdy]) continue;
-				if (chmin(initial[idx][jdy], initial[now.first][now.second] + 1))
-				{
-					q.emplace(idx, jdy);
-					chmax(furthest, initial[idx][jdy]);
-				}
-			}
-		}
-		chmax(ans, furthest);
-	}
-	cout << ans << endl;
+	cout << l << endl;
 }
 
 i64 modpow(i64 base, i64 ex, i64 mod)
@@ -136,18 +117,6 @@ template <class T> inline void show(const vector<T> &v)
 {
 	const i64 n = static_cast<i64>(v.size()) - 1;
 	for (i64 i = 0; i < n; i++)
-	{
-		if (v[i] == INF64)
-		{
-			cout << "# ";
-			continue;
-		}
 		cout << v[i] << " ";
-	}
-	if (v[n] == INF64)
-	{
-		cout << "#\n";
-		return;
-	}
 	cout << v[n] << "\n";
 }
